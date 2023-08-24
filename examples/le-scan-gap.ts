c3a602b949ce5ca6f22cd7ae6368e30dfe928267
Andrei Auchymmikau <kele6ra@gmail.com>
2023-08-24 12:05:04 +0300

    update for zephyr testing

--- examples/le-scan-gap.ts
+++ examples/le-scan-gap.ts
@@ -2,18 +2,30 @@ import { Utils } from './utils/Utils';
 
 import { Gap } from '../src/gap/Gap';
 import { Gatt } from '../src/gatt/Gatt';
-import { LeScanFilterDuplicates } from '../src/hci/HciLeController';
+import {
+  LeScanFilterDuplicates,
+  LeOwnAddressType,
+  LeScanningFilterPolicy,
+  LeScanType,
+} from '../src/hci/HciLeController';
 import { amendProfileWithUuidNames, uuidInfo } from '../src/utils/Profile';
 import { GattService } from '../src/gatt/GattService';
 import { GattCharacteristic } from '../src/gatt/GattCharacteristic';
+import { GattDescriptor } from '../src/gatt/GattDescriptor';
+import { AdvData } from '../src/gap/AdvData';
+
+const PDI_NOTIFY_SERVICE = '01000000000000000000000000000080';
+const PDI_NOTIFY_CHARACTERISTIC = '02000000000000000000000000000080';
+const PDI_WRITE_CHARACTERISTIC = '04000000000000000000000000000080';
 
 (async () => {
   try {
     const adapter = await Utils.createHciAdapter({
       deviceType: 'usb',
+      usb: { vid: 0x2fe3, pid: 0x000b },
       // usb: { vid: 0x2fe3, pid: 0x000d },
       // usb: { vid: 0x2fe3, pid: 0x000e },
-      usb: { vid: 0x0b05, pid: 0x190e }, // BT5
+      // usb: { vid: 0x0b05, pid: 0x190e }, // BT5
       // usb: { vid: 0x0b05, pid: 0x17cb }, // BT4
     });
     await Utils.defaultAdapterSetup(adapter.Hci);
@@ -21,8 +33,24 @@ import { GattCharacteristic } from '../src/gatt/GattCharacteristic';
     const gap = new Gap(adapter.Hci);
     await gap.init();
 
-    await gap.setScanParameters();
-    await gap.startScanning({ filterDuplicates: LeScanFilterDuplicates.Enabled });
+    await gap.setScanParameters({
+      ownAddressType: LeOwnAddressType.RandomDeviceAddress,
+      scanningFilterPolicy: LeScanningFilterPolicy.All,
+      scanningPhy: {
+        Phy1M: {
+          type: LeScanType.Passive,
+          intervalMs: 30,
+          windowMs: 30
+        },
+        PhyCoded: {
+          type: LeScanType.Passive,
+          intervalMs: 30,
+          windowMs: 30
+        },
+      },
+    });
+    await gap.startScanning({ filterDuplicates: LeScanFilterDuplicates.Enabled });  //comment for advertising test
+    // await gap.startScanning({ filterDuplicates: LeScanFilterDuplicates.Disabled }); //uncomment for advertising test
     console.log('scanning...');
 
     gap.on('GapLeScanState', (scanning) => {
@@ -30,19 +58,26 @@ import { GattCharacteristic } from '../src/gatt/GattCharacteristic';
     });
 
     let connecting = false;
+
+    adapter.Hci.on('LeExtendedAdvertisingReport', (report) => {
+      console.log("HCI report");
+      console.log(report.data);
+    });
+
     gap.on('GapLeAdvReport', async (report) => {
       if (connecting) { return; }
-      if (report.data.completeLocalName) {
-        console.log(report.address, report.data.completeLocalName, report.rssi, report.scanResponse);
-      }
-      if (report.data.completeLocalName !== 'Zephyr Heartrate Sensor') {
+
+      if (report.address.toString() !== '61:09:01:DE:00:42') {
         return;
       }
 
-      connecting = true;
-      console.log('connecting...');
-      await gap.stopScanning();
-      await gap.connect(report.address);
+      // console.log("Gap report"); //uncomment for advertising test
+      // console.log(report.address.toString(), report.data.completeLocalName, report.rssi, report.scanResponse); //uncomment for advertising test
+      // console.log(report.data); //uncomment for advertising test
+      connecting = true; //comment for advertising test
+      console.log('connecting...');  //comment for advertising test
+      await gap.stopScanning();  //comment for advertising test
+      await gap.connect(report.address);  //comment for advertising test
     });
 
     gap.on('GapConnected', async (event) => {
@@ -70,10 +105,10 @@ import { GattCharacteristic } from '../src/gatt/GattCharacteristic';
       const profileAmended = amendProfileWithUuidNames(profile);
 
       console.log(JSON.stringify(profileAmended, null, 2));
+      let nh: { service: GattService.AsObject, characteristic: GattCharacteristic.AsObject, descriptor: GattDescriptor.AsObject } | null = null;
 
-      let hr: { service: GattService.AsObject, characteristic: GattCharacteristic.AsObject } | null = null;
       for (const service of profileAmended.services) {
-        if (service.service.uuidInfo?.for !== 'Heart Rate') {
+        if (service.service.uuid !== PDI_NOTIFY_SERVICE) {
           continue;
         }
         for (const char of service.characteristics) {
@@ -81,41 +116,54 @@ import { GattCharacteristic } from '../src/gatt/GattCharacteristic';
             continue;
           }
           for (const descriptor of char.descriptors) {
-            if (descriptor.uuidInfo?.for !== 'Heart Rate Measurement') {
-              continue;
-            }
-            hr = { service: service.service, characteristic: char.characteristic };
+            nh = { service: service.service, characteristic: char.characteristic, descriptor: descriptor};
             break;
           }
-          if (hr) { break; }
+          if (nh) { break; }
         }
-        if (hr) { break; }
+        if (nh) { break; }
       }
 
-      if (hr) {
-        await gatt.startCharacteristicsNotifications(hr.characteristic, false);
+      console.log(nh);
+
+      if (nh) {
+        await gatt.startCharacteristicsNotifications(nh.characteristic, false);
         gatt.on('GattNotification', (s, c, d, b) => {
-          const flags = b[0];
-          const v16 = flags & 1;
-          const value = v16 ? b.readUIntLE(1, 2) : b.readUIntLE(1, 1);
           console.log(`
             ${s.uuid}, ${uuidInfo(s.uuid)?.for}
+            ${c.uuid}, ${uuidInfo(c.uuid)?.for}
             ${d.uuid}, ${uuidInfo(d.uuid)?.for}
-            ${value} bpm
+            buffer ${b.toString('hex')}
           `);
         });
+
       }
 
       setTimeout(async () => {
         await gap.disconnect(event.connectionHandle);
-      }, 10_000);
+      }, 60_000);
     });
 
     gap.on('GapDisconnected', async (reason) => {
       connecting = false;
       console.log('DisconnectionComplete', reason);
 
-      await gap.setScanParameters();
+      await gap.setScanParameters({
+        ownAddressType: LeOwnAddressType.RandomDeviceAddress,
+        scanningFilterPolicy: LeScanningFilterPolicy.All,
+        scanningPhy: {
+          Phy1M: {
+            type: LeScanType.Active,
+            intervalMs: 30,
+            windowMs: 30
+          },
+          PhyCoded: {
+            type: LeScanType.Active,
+            intervalMs: 30,
+            windowMs: 30
+          },
+        },
+      });
       await gap.startScanning({ filterDuplicates: LeScanFilterDuplicates.Enabled });
     });
   } catch (e) {
